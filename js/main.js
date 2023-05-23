d1c521e0b155f0b06ad282e787ec4678979b5769
Alex Jenkinson <alexjenkinson@ideasquared.co.uk>
2023-05-23 22:37:35 +0100

    FIX: Testing CDN import to fix install issue

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 // This includes Popper and all of Bootstrap's JS plugins.
 
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
-
+import bootstrap from 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/+esm';
 
 //
 // Place any custom JS here
